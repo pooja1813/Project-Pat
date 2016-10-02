5a96500f061d6d7ec004b936f4dd2b2928e006c5
pooja1813 <pooja.sampelly23@gmail.com>
2016-10-02 05:26:19 -0400

    Retrieved Data from api

---
+++ js/app-module.js
@@ -0,0 +1,39 @@
+
+'use strict';
+
+angular.module('patientsApp',['ngRoute'])
+	.config(function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl:'view/patientsList.html',
+		controller: 'patientsCtrl'
+	})
+	.when('/create',{
+		templateUrl:'view/create-patient.html',
+		controller:'patientsCtrl'
+	})
+	}
+	.when('/:id',{
+		templateUrl:'view/patient-view.html',
+		controller:'patientsCtrl'
+	})
+	.when('/:id/update',{
+		templateUrl:'view/update-patient.html',
+		controller: 'patientsCtrl'
+		method:'update'
+	})
+	.when('/:id/delete',{
+		templateUrl:'view/delete-patient.html',
+		controller: 'patientsCtrl'
+		method:'delete'
+	})
+	.when('/deleteall',{
+		templateUrl:'view/deleteall',
+		controller:'patientsCtrl',
+		method:'deleteAll'
+	})
+    .otherwise({
+		redirectTo:'/'
+	});
+	});
+
+}]);
---
+++ js/patientsFactory.js
@@ -0,0 +1,30 @@
+angular.module('patientsApp')
+    .factory('patientsFactory', ['$http', function($http) {
+
+    var baseUrl = 'https://izenda.herokuapp.com/patients';
+    var patientsFactory = {};
+
+    patientsFactory.getPatients = function () {
+
+        return $http.get(baseUrl);
+    };
+
+    patientsFactory.getPatient = function (id) {
+        return $http.get(baseUrl + '/' + id);
+    };
+
+    patientsFactory.createPatient = function (pat) {
+        return $http.post(baseUrl, pat);
+    };
+
+    patientsFactory.updatePatient = function (pat) {
+        return $http.put(baseUrl + '/' + pat.ID, pat)
+    };
+
+    patientsFactory.deletePatient = function (id) {
+        return $http.delete(baseUrl + '/' + id);
+    };
+
+
+    return patientsFactory;
+}]);
