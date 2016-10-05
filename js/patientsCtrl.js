502f9d585a24f25db9141fef4324f5d7f6a62876
pooja1813 <pooja.sampelly23@gmail.com>
2016-10-05 09:03:05 -0400

    code clean up

--- js/patientsCtrl.js
+++ js/patientsCtrl.js
@@ -69,6 +69,7 @@ app.config(['$routeProvider',
             $scope.status = 'Error: Unable to delete patient data: ' + error.message;
         });
     };
+
 }]);
 
 app.controller('detailsCtrl', ['$scope','$http','$routeParams',
